8dfaffefbde933a6dd650353e501f8df8588a298
Lukas Becker <lujebecker@googlemail.com>
2021-04-01 17:00:44 +0200

    Delete get_species_taxid.py

--- scripts/get_species_taxid.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-try:
-    pid = subprocess.Popen(['get_species_taxids.sh','-n','Eubacteria','>','eubacteria.taxid'])
-    print("[+] spawned new process: {}".format(pid.pid))
-    signal = pid.communicate()
-    print("[*] waiting for get_species_taxids.sh to finish: {}".format(signal))
-except Exception as e:
-    print("[-] Exception occured: {}".format(e))
